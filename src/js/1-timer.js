27eda96de96b746159820e85aa541fc83ef082dc
Stepannovv <stepannovv@gmail.com>
2024-06-30 17:14:42 +0200

    mistake

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
+import 'flatpickr/dist/flatpickr.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const datetimePicker = document.getElementById('datetime-picker');
